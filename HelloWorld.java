561884a8c8c9d1fc84bbaef76152bdcf2f6a47ee
gxchuminnan <540997019@qq.com>
2014-02-07 11:12:07 +0800

    1
    打印出“****”

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,6 +1,10 @@
 public class HelloWorld{
   public static void main(String[] args)
   {
+   System.out.println("***");
+   System.out.println("***");
    System.out.println("HWq!");
+   System.out.println("***");
+   
    }
 }
